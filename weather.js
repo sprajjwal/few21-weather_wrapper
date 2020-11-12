1522780982317609c6fcdab44515d87bdab14377
sprajjwal <sprajjwal@ucdavis.edu>
2020-11-12 15:56:49 -0800

    files sepearated

---
+++ weather.js
@@ -0,0 +1,29 @@
+// ------------------------------------------------------
+      // Developer Code
+
+      // Get Element refererences
+      const tempEl = document.getElementById('temp')
+      const descEl = document.getElementById('desc')
+      const formEl = document.getElementById('form')
+      const zipInput = document.getElementById('zip')
+
+      // Define event listeners
+      formEl.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const zip = zipInput.value
+        getWeather(zip)
+      })
+
+      // Functions 
+      function getWeather(zip) {
+        const apiKey = '467355df4c808dd6134a3b64e9ace282'
+        const units = 'imperial'
+        const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+        fetch(path)
+          .then(res => res.json())
+          .then(json => {
+            temp.innerHTML = json.main.temp
+            desc.innerHTML = json.weather[0].description
+          })
+          .catch(err => console.log(err.message))
+      }
