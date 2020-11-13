32187692606fc0f98b02d918d19b8cff18b902d4
sprajjwal <sprajjwal@ucdavis.edu>
2020-11-12 16:59:38 -0800

    challenge 5 done

--- weather.js
+++ weather.js
@@ -20,11 +20,12 @@ formEl.addEventListener('submit', (e) => {
 })
 
 // Functions 
-function getWeather(apiKey, zip) {
+async function getWeather(apiKey, zip) {
     const units = 'imperial'
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-    return fetch(path)
-        .then(res => res.json())
+    const res = await fetch(path)
+    const payload = await res.json()
+    return payload
         // .then(json => {
         //     callback(json)
         // })
